8becf8e5897af7a86e348ca07efe8092c73743b1
Alexandre Touret <alexandre.touret@free.fr>
2017-10-16 11:30:02 -0400

    correction password

--- scripts/create-hadoop-hdinsight.sh
+++ scripts/create-hadoop-hdinsight.sh
@@ -2,5 +2,5 @@
 PREFIX= $1
 KEY=$2 
 
- azure hdinsight cluster create -g $PREFIXgroup -l westeurope -y Linux --clusterType Hadoop --defaultStorageAccountName $PREFIXstorage.blob.core.windows.net --defaultStorageAccountKey $KEY --defaultStorageContainer $PREFIXcluster --workerNodeCount 3 --userName admin --password sihM1234 --sshUserName sshuser --sshPassword  sihM1234 $PREFIXcluster
+azure hdinsight cluster create -g $PREFIXgroup -l westeurope -y Linux --clusterType Hadoop --defaultStorageAccountName $PREFIXstorage.blob.core.windows.net --defaultStorageAccountKey $KEY --defaultStorageContainer $PREFIXcluster --workerNodeCount 3 --userName admin --password passworD1 --sshUserName sshuser --sshPassword   passworD1 $PREFIXcluster
 
